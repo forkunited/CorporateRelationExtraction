98939bf93453799000475cb443d07ec146e3c3d3
Bill McDowell <forkunited@gmail.com>
2014-03-09 20:48:21 -0400

    ...

--- src/corp/model/ModelAdaGrad.java
+++ src/corp/model/ModelAdaGrad.java
@@ -131,10 +131,14 @@ public class ModelAdaGrad extends Model {
 					this.feature_G[i] += feature_g[i]*feature_g[i];
 					this.feature_u[i] += feature_g[i];
 					
-					if (Math.abs(this.feature_u[i])/this.t <= lambda_1)
-						this.feature_w[i] = 0; 
-					else 
-						this.feature_w[i] = -Math.signum(this.feature_u[i])*(this.t/(Math.sqrt(this.feature_G[i])))*((Math.abs(this.feature_u[i])/this.t)-lambda_1); 
+					// FIXME: Replace this with regularized version later
+					if (this.feature_G[i] != 0)
+						this.feature_w[i] -= feature_g[i]/Math.sqrt(this.feature_G[i]); 
+					
+					//if (Math.abs(this.feature_u[i])/this.t <= lambda_1)
+					//	this.feature_w[i] = 0; 
+					//else 
+					//	this.feature_w[i] = -Math.signum(this.feature_u[i])*(this.t/(Math.sqrt(this.feature_G[i])))*((Math.abs(this.feature_u[i])/this.t)-lambda_1); 
 				}
 				
 				// Update cost weights
